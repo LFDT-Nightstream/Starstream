5a8db0a24c727f4b89a3ef96adbb91dfaf5d4d68
Las <me@las.rs>
2025-08-19 17:50:58 +0000

    starstream_nova: Add (yet unused) abstraction over circuit building

--- starstream_nova/src/lib.rs
+++ starstream_nova/src/lib.rs
@@ -1,18 +1,48 @@
 use ff::{PrimeField, PrimeFieldBits};
-use nova::frontend::PoseidonConstants;
+use nova::frontend::test_cs::TestConstraintSystem;
+use std::marker::PhantomData;
+
 use nova::frontend::gadgets::poseidon::poseidon_hash_allocated;
 use nova::frontend::{ConstraintSystem, SynthesisError, num::AllocatedNum};
+use nova::frontend::{LinearCombination, PoseidonConstants, Variable};
 use nova::nebula::rs::StepCircuit;
+use std::ops::{Add, Mul};
 use std::sync::{Arc, Mutex};
 use typenum::U4;
+
+#[derive(Clone, Copy, Default)]
+pub struct Location {
+    label: &'static str,
+    file: &'static str,
+    line: u32,
+    column: u32,
+}
+
+pub enum Locations<'a> {
+    Nil,
+    Cons(Location, &'a Locations<'a>),
+}
+
+fn from_location(
+    Location {
+        label,
+        file,
+        line,
+        column,
+    }: Location,
+) -> String {
+    format!("{}@{}:{}:{}", label, file, line, column)
+}
 
 // FIXME: horrifying
 static CONSTRAINT_COUNTER: Mutex<i64> = Mutex::new(0);
 
+// FIXME: Use Once to only format once
 macro_rules! label_ {
     () => {{ || format!("{}:{}:{}", file!(), line!(), column!()).replace("/", ".") }};
 }
 
+// FIXME: Use Once to only format once
 macro_rules! label {
     () => {{
         || {
@@ -30,17 +60,303 @@ macro_rules! nest {
 
 macro_rules! alloc {
     ($name:literal, $cs:expr, $w:expr) => {{
-        match $w.get($name, label_!()) {
-            Some(val) => AllocatedNum::alloc_infallible(nest!($cs), || val),
-            None => AllocatedNum::alloc_infallible(nest!($cs), || {
-                unreachable!("witness requested when unavailable")
-            }),
-        }
+        let location = Location {
+            label: $name,
+            file: file!(),
+            line: line!(),
+            column: column!(),
+        };
+        let val = $w.get(Locations::Cons(location, &Locations::Nil));
+        AllocatedNum::alloc_infallible(nest!($cs), || val)
     }};
 }
 
-trait Witness<F>: Sync {
-    fn get(&mut self, name: &'static str, label: impl FnOnce() -> String) -> Option<F>;
+pub trait Witness<F>: Sync + Send {
+    fn get(&mut self, locations: Locations<'_>) -> F;
+}
+
+trait CircuitBuilder<'a> {
+    type F: PrimeField;
+    type Var: Add<Self::Var, Output = Self::Var> + Mul<u128, Output = Self::Var>;
+
+    fn zero(&mut self) -> Self::Var;
+    fn one(&mut self) -> Self::Var;
+    fn lit(&mut self, n: u128) -> Self::Var;
+    fn alloc(&mut self, location: Location) -> Self::Var;
+    fn enforce(&mut self, location: Location, a: Self::Var, b: Self::Var, a_times_b: Self::Var);
+    fn nest<'b: 'a>(&'b mut self, location: Location) -> impl CircuitBuilder<'b, Var = Self::Var>;
+    fn from_allocated_num(&mut self, var: AllocatedNum<Self::F>) -> Self::Var;
+    fn to_allocated_num(&mut self, var: Self::Var) -> AllocatedNum<Self::F>;
+}
+
+trait Circuit: Send + Sync {
+    fn run<'a, CB: CircuitBuilder<'a>>(&self, builder: CB, io: Vec<CB::Var>) -> Vec<CB::Var>;
+}
+
+fn test_circuit<F: PrimeField + PrimeFieldBits, W: Witness<F>, C: Circuit>(
+    io: Vec<F>,
+    circuit: C,
+    witness: W,
+) {
+    struct Builder<'a, F, W, CS> {
+        cs: CS,
+        locations: Locations<'a>,
+        witness: &'a mut W,
+        _f: PhantomData<F>,
+    }
+
+    struct Single<F> {
+        inner: Variable,
+        scale: F,
+        data: Option<F>,
+    }
+
+    enum VarImpl<F> {
+        Zero,
+        VarImpl(Vec<Single<F>>),
+    }
+
+    fn to_lc<F: PrimeField>(lc: LinearCombination<F>, var: VarImpl<F>) -> LinearCombination<F> {
+        match var {
+            VarImpl::Zero => LinearCombination::zero(),
+            VarImpl::VarImpl(vars) => vars.into_iter().fold(
+                lc,
+                |lc,
+                 Single {
+                     inner,
+                     scale,
+                     data: _,
+                 }| lc + (scale, inner),
+            ),
+        }
+    }
+
+    fn var_impl_data<F: PrimeField>(var: VarImpl<F>) -> Option<F> {
+        match var {
+            VarImpl::Zero => Some(F::ZERO),
+            VarImpl::VarImpl(vec) => vec.into_iter().fold(
+                Some(F::ZERO),
+                |acc,
+                 Single {
+                     inner: _,
+                     scale: _,
+                     data,
+                 }| {
+                    match (acc, data) {
+                        (Some(acc), Some(data)) => Some(acc * data),
+                        _ => None,
+                    }
+                },
+            ),
+        }
+    }
+
+    impl<F: PrimeField> Add<VarImpl<F>> for VarImpl<F> {
+        type Output = VarImpl<F>;
+
+        fn add(self, rhs: VarImpl<F>) -> VarImpl<F> {
+            match (self, rhs) {
+                (VarImpl::Zero, rhs) => rhs,
+                (lhs, VarImpl::Zero) => lhs,
+                (VarImpl::VarImpl(lhs), VarImpl::VarImpl(rhs)) => {
+                    VarImpl::VarImpl(lhs.into_iter().chain(rhs.into_iter()).collect())
+                }
+            }
+        }
+    }
+    impl<F: PrimeField> Mul<u128> for VarImpl<F> {
+        type Output = VarImpl<F>;
+
+        fn mul(self, rhs: u128) -> VarImpl<F> {
+            match self {
+                VarImpl::Zero => VarImpl::Zero,
+                VarImpl::VarImpl(lhs) => VarImpl::VarImpl(
+                    lhs.into_iter()
+                        .map(|Single { inner, scale, data }| {
+                            let rhs = F::from_u128(rhs);
+                            Single {
+                                inner,
+                                scale: scale * rhs,
+                                data: data.map(|d| d * rhs),
+                            }
+                        })
+                        .collect(),
+                ),
+            }
+        }
+    }
+
+    impl<'a, F: PrimeField, W: Witness<F>, CS: ConstraintSystem<F>> CircuitBuilder<'a>
+        for Builder<'a, F, W, CS>
+    {
+        type F = F;
+        type Var = VarImpl<F>;
+        fn zero(&mut self) -> VarImpl<F> {
+            VarImpl::Zero
+        }
+        fn one(&mut self) -> VarImpl<F> {
+            VarImpl::VarImpl(vec![Single {
+                inner: CS::one(),
+                scale: F::ONE,
+                data: Some(F::ONE),
+            }])
+        }
+        fn lit(&mut self, n: u128) -> VarImpl<F> {
+            let n = F::from_u128(n);
+            VarImpl::VarImpl(vec![Single {
+                inner: CS::one(),
+                scale: n,
+                data: Some(n),
+            }])
+        }
+        fn alloc(&mut self, location: Location) -> VarImpl<F> {
+            let w = self.witness.get(Locations::Cons(location, &self.locations));
+            let var = self
+                .cs
+                .alloc(|| "CircuitBuilder::alloc", || Ok(w))
+                .expect("impossible");
+            VarImpl::VarImpl(vec![Single {
+                inner: var,
+                scale: F::ONE,
+                data: Some(w),
+            }])
+        }
+        fn enforce(
+            &mut self,
+            location: Location,
+            a: VarImpl<F>,
+            b: VarImpl<F>,
+            a_times_b: VarImpl<F>,
+        ) {
+            self.cs.enforce(
+                || from_location(location),
+                |lc| to_lc(lc, a),
+                |lc| to_lc(lc, b),
+                |lc| to_lc(lc, a_times_b),
+            );
+        }
+        fn nest<'b: 'a>(
+            &'b mut self,
+            location: Location,
+        ) -> Builder<'b, F, W, impl ConstraintSystem<F>> {
+            Builder {
+                cs: self.cs.namespace(|| from_location(location)),
+                locations: Locations::Cons(location, &self.locations),
+                witness: self.witness,
+                _f: PhantomData,
+            }
+        }
+        fn from_allocated_num(&mut self, var: AllocatedNum<F>) -> VarImpl<F> {
+            VarImpl::VarImpl(vec![Single {
+                inner: var.get_variable(),
+                scale: F::ONE,
+                data: var.get_value(),
+            }])
+        }
+        fn to_allocated_num(&mut self, var: Self::Var) -> AllocatedNum<Self::F> {
+            AllocatedNum::alloc(self.cs.namespace(|| "to_allocated_num"), || {
+                var_impl_data(var).ok_or(SynthesisError::AssignmentMissing)
+            })
+            .expect("to_allocated_num; shouldn't happen")
+        }
+    }
+
+    struct StepCircuitImpl<C, W> {
+        arity: usize,
+        circuit: Arc<C>,
+        witness: Arc<Mutex<W>>,
+    }
+
+    impl<C, W> Clone for StepCircuitImpl<C, W> {
+        fn clone(&self) -> Self {
+            let StepCircuitImpl {
+                arity,
+                circuit,
+                witness,
+            } = self;
+            let arity = *arity;
+            let circuit = circuit.clone();
+            let witness = witness.clone();
+            StepCircuitImpl {
+                arity,
+                circuit,
+                witness,
+            }
+        }
+    }
+
+    impl<F, C: Circuit, W: Witness<F>> StepCircuit<F> for StepCircuitImpl<C, W>
+    where
+        F: PrimeField + PrimeFieldBits,
+    {
+        fn arity(&self) -> usize {
+            self.arity
+        }
+
+        fn synthesize<CS: ConstraintSystem<F>>(
+            &self,
+            cs: &mut CS,
+            io: &[AllocatedNum<F>],
+        ) -> Result<Vec<AllocatedNum<F>>, SynthesisError> {
+            let mut w_guard = self.witness.lock().unwrap();
+
+            let w = &mut *w_guard;
+
+            let io = {
+                let mut builder = Builder {
+                    cs: cs.namespace(|| "internal"),
+                    locations: Locations::Nil,
+                    witness: w,
+                    _f: PhantomData,
+                };
+
+                io.iter()
+                    .map(|var| builder.from_allocated_num(var.clone()))
+                    .collect()
+            };
+
+            let io = self.circuit.run(
+                Builder {
+                    cs: cs.namespace(|| "circuit"),
+                    locations: Locations::Nil,
+                    witness: w,
+                    _f: PhantomData,
+                },
+                io,
+            );
+            let mut builder = Builder {
+                cs: cs.namespace(|| "internal"),
+                locations: Locations::Nil,
+                witness: w,
+                _f: PhantomData,
+            };
+
+            Ok(io
+                .into_iter()
+                .map(|var| builder.to_allocated_num(var))
+                .collect())
+        }
+
+        fn non_deterministic_advice(&self) -> Vec<F> {
+            Vec::new()
+        }
+    }
+    let mut test = TestConstraintSystem::new();
+    let im = StepCircuitImpl {
+        arity: io.len(),
+        witness: Arc::new(Mutex::new(witness)),
+        circuit: Arc::new(circuit),
+    };
+    let io: Vec<_> = io
+        .iter()
+        .map(|v| AllocatedNum::alloc_infallible(&mut test, || *v))
+        .collect();
+    im.synthesize(&mut test, &io).expect(label_!()().as_ref());
+    eprintln!(
+        "printing unsatisfied constraints\n{:?}\nprinting unsatisfied constraints done",
+        test.which_is_unsatisfied()
+    );
+    assert!(test.is_satisfied());
 }
 
 struct Switches<F: PrimeField>(Vec<AllocatedNum<F>>);
@@ -557,7 +873,6 @@ where
 #[ignore]
 fn prove_dummy() {
     use ff::Field;
-    use nova::frontend::test_cs::TestConstraintSystem;
     use nova::nebula::rs::{PublicParams, RecursiveSNARK};
     use nova::provider::PallasEngine;
     use nova::traits::{Engine, snark::default_ck_hint};
@@ -565,11 +880,10 @@ fn prove_dummy() {
     use std::fs::File;
     use std::io::{BufRead, BufReader};
     use std::path::Path;
-
     struct WitnessFromFile(BufReader<File>);
 
     impl<F: PrimeField> Witness<F> for WitnessFromFile {
-        fn get(&mut self, name: &'static str, label: impl FnOnce() -> String) -> Option<F> {
+        fn get(&mut self, name: &'static str, label: impl Fn() -> String) -> Option<F> {
             let mut out = String::new();
             let _ = self.0.read_line(&mut out);
             let mut split = out.split_ascii_whitespace();
@@ -598,7 +912,7 @@ fn prove_dummy() {
     struct NoWitness;
 
     impl<F> Witness<F> for NoWitness {
-        fn get(&mut self, _name: &'static str, _label: impl FnOnce() -> String) -> Option<F> {
+        fn get(&mut self, _name: &'static str, _label: impl Fn() -> String) -> Option<F> {
             None
         }
     }
@@ -624,7 +938,7 @@ fn prove_dummy() {
     StarstreamCircuit(Arc::new(Mutex::new(witness())))
         .synthesize(&mut test, &allocated_input)
         .expect(label_!()().as_ref());
-    println!(
+    eprintln!(
         "printing unsatisfied constraints\n{:?}\nprinting unsatisfied constraints done",
         test.which_is_unsatisfied()
     );
