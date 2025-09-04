7edca7d74b6a837f069f992ecba7b0c0a9d14fb2
Tad Hardesty <tad@dcspark.io>
2025-09-04 11:39:39 -0700

    vscode: Use `cargo run` to start the language server

--- starstream_vscode/src/extension.ts
+++ starstream_vscode/src/extension.ts
@@ -1,11 +1,18 @@
+import { realpath } from "node:fs/promises";
 import * as vscode from "vscode";
 import { LanguageClient, TransportKind } from "vscode-languageclient/node";
 
 export async function activate(context: vscode.ExtensionContext) {
+  // TODO for publishing: bundle the LSP executable as part of the extension.
+  // For now, we `cargo run` it assuming this extension is running out of its source directory.
   const lc = new LanguageClient(
     "starstream-lsp",
     {
-      command: context.asAbsolutePath("../target/debug/starstream_lsp"),
+      command: "cargo",
+      args: ["run", "-p", "starstream_lsp", "--"],
+      options: {
+        cwd: `${await realpath(context.extensionPath)}/..`,
+      },
       transport: TransportKind.stdio,
     },
     {
