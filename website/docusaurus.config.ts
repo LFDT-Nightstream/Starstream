f07aa3b963f5549b57cf637e8e857673e6efde01
Tad Hardesty <tad@dcspark.io>
2025-10-14 11:55:18 -0700

    Update links and CI to refer to 'main' branch

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           routeBasePath: "/",
           sidebarPath: "./sidebars.ts",
           editUrl:
-            "https://github.com/PaimaStudios/Starstream/tree/master/website/",
+            "https://github.com/PaimaStudios/Starstream/tree/main/website/",
         },
         blog: {
           showReadingTime: false,
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/PaimaStudios/Starstream/tree/master/website/",
+            "https://github.com/PaimaStudios/Starstream/tree/main/website/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
