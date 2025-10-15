f1f054adcd674579f0ab865a1eb16042b8ddbd31
Tad Hardesty <tad@dcspark.io>
2025-10-15 15:54:31 -0700

    Add more compiler scaffolding

--- new-starstream/starstream-dsl/starstream-to-wasm/src/lib.rs
+++ new-starstream/starstream-dsl/starstream-to-wasm/src/lib.rs
@@ -1,9 +1,10 @@
-use starstream_types::Program;
+use starstream_types::*;
 use wasm_encoder::*;
 
 /// Compile a Starstream program to a Wasm module.
-pub fn compile<'a>(program: &'a Program) -> Vec<u8> {
-    let compiler = Compiler::default();
+pub fn compile(program: &Program) -> Vec<u8> {
+    let mut compiler = Compiler::default();
+    compiler.visit_program(program);
     compiler.finish()
 }
 
@@ -22,7 +23,7 @@ struct Compiler {
 
 impl Compiler {
     fn finish(mut self) -> Vec<u8> {
-        Default::default()
+        self.to_module().finish()
     }
 
     fn to_module(&self) -> Module {
@@ -57,5 +58,70 @@ impl Compiler {
             module.section(&self.data);
         }
         module
+    }
+
+    fn visit_program(&mut self, program: &Program) {
+        let mut main = Function::default();
+        self.visit_block(&mut main, &program.statements);
+    }
+
+    fn visit_block(&mut self, func: &mut Function, block: &[Statement]) {
+        for statement in block {
+            self.visit_statement(func, statement);
+        }
+    }
+
+    fn visit_statement(&mut self, func: &mut Function, statement: &Statement) {
+        todo!()
+    }
+
+    fn visit_expr(&mut self, func: &mut Function, expr: &Expr) {
+        todo!()
+    }
+}
+
+/// A replacement for [wasm_encoder::Function] that allows adding locals gradually.
+#[derive(Default)]
+pub struct Function {
+    num_locals: u32,
+    locals: Vec<(u32, ValType)>,
+    bytes: Vec<u8>,
+}
+
+impl Function {
+    fn new(params: &[ValType]) -> Function {
+        let mut this = Function::default();
+        for param in params {
+            this.add_local(*param);
+        }
+        this
+    }
+
+    fn add_local(&mut self, ty: ValType) -> u32 {
+        let id = self.num_locals;
+        self.num_locals += 1;
+        if let Some((last_count, last_type)) = self.locals.last_mut() {
+            if ty == *last_type {
+                *last_count += 1;
+                return id;
+            }
+        }
+        self.locals.push((1, ty));
+        id
+    }
+
+    fn instructions(&mut self) -> InstructionSink {
+        InstructionSink::new(&mut self.bytes)
+    }
+}
+
+impl wasm_encoder::Encode for Function {
+    fn encode(&self, sink: &mut Vec<u8>) {
+        self.locals.len().encode(sink);
+        for (count, ty) in &self.locals {
+            count.encode(sink);
+            ty.encode(sink);
+        }
+        sink.extend_from_slice(&self.bytes);
     }
 }
