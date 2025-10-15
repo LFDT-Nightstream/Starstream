78c278b057267704b4f24d364e6700082e428d3c
SpaceManiac <tad@dcspark.io>
2025-10-15 12:07:14 -0700

    Add website folder to new-starstream (#55)
    Adds the stub of a documentation website:
    - Based on the Docusaurus v2 "classic" template, but with all the dummy
    content files removed
    - No landing page, just goes straight to the index of the docs
    - Adds stub root `README.md` and references it by `import` in the docs
    index
    - Includes [docusaurus-plugin-generate-llms-txt]
    - However, this isn't well compatible with the .mdx `import` style, so
    perhaps that should not be used
    
    Closes #54.
    
    <img width="1006" height="691" alt="image"
    src="https://github.com/user-attachments/assets/1ed2e42c-9efa-42d2-ac7d-1f69b897d130"
    />
    
    [docusaurus-plugin-generate-llms-txt]:
    https://www.npmjs.com/package/docusaurus-plugin-generate-llms-txt

---
+++ new-starstream/website/docusaurus.config.ts
@@ -0,0 +1,174 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Starstream",
+  //tagline: "Dinosaurs are cool",
+  //favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://paimastudios.github.io/",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/Starstream/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "PaimaStudios", // Usually your GitHub org/user name.
+  projectName: "Starstream", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenAnchors: "throw",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl: "https://github.com/PaimaStudios/Starstream/tree/main/website/",
+        },
+        /*
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/PaimaStudios/Starstream/tree/main/website/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        */
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "Starstream",
+      /*logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },*/
+      items: [
+        {
+          href: "https://github.com/PaimaStudios/Starstream",
+          label: "GitHub",
+        },
+        {
+          href: "https://discord.com/invite/9eZaheySZE",
+          label: "Discord",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Documentation",
+        },
+        /*
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right",
+        },
+        */
+      ],
+    },
+    footer: {
+      style: "dark",
+      /*links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "X",
+              href: "https://x.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],*/
+      copyright: `Copyright © ${new Date().getFullYear()} Paima Studios LTD.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+
+  plugins: [
+    /*
+      Note: This plugin makes some assumptions about the structure of your docs:
+      - The docs directory contains your documentation
+      - Each category has a _category_.yml file that contains the category metadata
+      - Each page has frontmatter metadata
+      - For top-level Markdown pages, there is a sidebar_position field in the metadata
+    */
+    "docusaurus-plugin-generate-llms-txt",
+  ],
+};
+
+export default config;
