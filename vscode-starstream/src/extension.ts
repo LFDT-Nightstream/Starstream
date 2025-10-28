04715937e9e0edee125c1a7415970aceb348e901
Tad Hardesty <tad.hardesty@midnight.foundation>
2025-10-28 15:41:52 -0700

    VSC: Remove duplicate tree-sitter.wasm include
    Signed-off-by: Tad Hardesty <tad.hardesty@midnight.foundation>

--- vscode-starstream/src/extension.ts
+++ vscode-starstream/src/extension.ts
@@ -1,7 +1,6 @@
 import { Parser } from "web-tree-sitter";
 import * as vscode from "vscode";
 import { LanguageClient, TransportKind } from "vscode-languageclient/node";
-import tree_sitter_wasm from "file-loader!../node_modules/web-tree-sitter/tree-sitter.wasm";
 import tree_sitter_starstream_wasm from "file-loader!../../tree-sitter-starstream/tree-sitter-starstream.wasm";
 import highlights_scm from "raw-loader!../../tree-sitter-starstream/queries/highlights.scm";
 import { realpath } from "fs/promises";
@@ -48,15 +47,7 @@ async function activateLanguageClient(context: vscode.ExtensionContext) {
 // Tree-sitter highlighter
 
 async function activateTreeSitter(context: vscode.ExtensionContext) {
-  const fullTsWasm = await resolve(context, tree_sitter_wasm);
-  await Parser.init({
-    locateFile(name: string, _dir: string): string | null {
-      if (name === "tree-sitter.wasm") {
-        return fullTsWasm;
-      }
-      return null;
-    },
-  });
+  await Parser.init();
 
   // Use an embedded copy of https://marketplace.visualstudio.com/items?itemName=AlecGhost.tree-sitter-vscode
   // Until https://github.com/microsoft/vscode/issues/50140
