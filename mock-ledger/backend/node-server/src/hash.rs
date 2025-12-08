6f1072e0e8b03337ade543a78be59afabec2cbb8
Sebastien Guillemot <SebastienGllmt@gmail.com>
2025-12-08 16:17:43 +0900

    improve hash function

--- mock-ledger/backend/node-server/src/hash.rs
+++ mock-ledger/backend/node-server/src/hash.rs
@@ -1,30 +1,42 @@
 use p3_goldilocks::{Goldilocks, Poseidon2Goldilocks};
+use p3_field::{PrimeCharacteristicRing};
 use p3_symmetric::PaddingFreeSponge;
-use p3_symmetric::CryptographicHasher; // the trait
-use p3_field::PrimeCharacteristicRing;
+use p3_symmetric::CryptographicHasher;
 use rand::{rngs::StdRng, SeedableRng};
+use once_cell::sync::Lazy;
 
-const WIDTH: usize = 8;
-const RATE: usize = 4;      // must be < WIDTH
-const OUT: usize = 1;       // number of field elements in the digest
+// capacity = WIDTH - RATE = 4 bytes = 256 bytes of pre-image security = 128 bits of collision resistance
+const WIDTH: usize = 8; // recommended parameter
+const RATE: usize = 4;  // Run permutation on 4 elements at a time (~256 bits). Must be < WIDTH
+const OUT: usize = 4;   // 4*64= ~256 bit output (slightly lower as goldilocks is not quite 64-bits)
+
+static HASHER: Lazy<Hash> = Lazy::new(|| {
+  // constant seed so contract hashes are deterministic
+  let mut rng = StdRng::seed_from_u64(0);
+  let perm = Perm::new_from_rng_128(&mut rng);
+  Hash::new(perm)
+});
 
 type Perm = Poseidon2Goldilocks<WIDTH>;
 type Hash = PaddingFreeSponge<Perm, WIDTH, RATE, OUT>;
 
-fn make_hasher() -> Hash {
-    // Deterministic RNG → deterministic Poseidon2 parameters
-    let mut rng = StdRng::seed_from_u64(0);
-    let perm = Perm::new_from_rng_128(&mut rng);
-    Hash::new(perm)
-}
+/**
+ * Outputs a 256-bit hash from input bytes.
+ */
+pub fn poseidon2_hash_bytes(bytes: &[u8]) -> [Goldilocks; OUT] {
+  let field_iter = bytes
+        // Goldilocks field modulus: p = 2^64 - 2^32 + 1
+        // therefore 8 bytes may not fit, so we use 7 instead
+        .chunks(7)
+        .map(|chunk| {
+            // Interpret up to 8 bytes as little-endian u64
+            let mut buf = [0u8; 8];
+            // Interpret up to 7 bytes as little-endian u64,
+            // padded with zeros in the high bytes.
+            buf[..chunk.len()].copy_from_slice(chunk);
+            let word = u64::from_le_bytes(buf);
+            Goldilocks::from_u64(word)
+        });
 
-pub fn poseidon2_hash_bytes(s: &[u8]) -> [Goldilocks; OUT] {
-  let hasher = make_hasher();
-
-  // Map each byte → Goldilocks field element
-  let field_iter = s.iter().map(|b| Goldilocks::from_u8(*b));
-
-  // PaddingFreeSponge implements CryptographicHasher<T, [T; OUT]>
-  // where T = Goldilocks here.
-  hasher.hash_iter(field_iter)
+  HASHER.hash_iter(field_iter)
 }
