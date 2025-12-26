902252b921dc57dfecb79bf44d36de2412b89d7f
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2025-12-26 15:29:12 -0300

    mock_ledger_spec: git add missing files and add more tests/simplifications
    Signed-off-by: Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>

---
+++ starstream_mock_ledger/src/transaction_effects/witness.rs
@@ -0,0 +1,68 @@
+use crate::{
+    Hash, Value, WasmModule,
+    transaction_effects::{InterfaceId, ProcessId},
+};
+
+/// One entry in the per-process trace.
+//
+// Note that since these are witnesses, they include the inputs and the outputs
+// for each operation.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum WitLedgerEffect {
+    Resume {
+        target: ProcessId,
+        val: Value,
+        ret: Value,
+        id_prev: Option<ProcessId>,
+    },
+    Yield {
+        val: Value,
+        ret: Option<Value>,
+        id_prev: Option<ProcessId>,
+    },
+    ProgramHash {
+        target: ProcessId,
+        program_hash: Hash<WasmModule>,
+    },
+    NewUtxo {
+        program_hash: Hash<WasmModule>,
+        val: Value,
+        id: ProcessId,
+    },
+    NewCoord {
+        program_hash: Hash<WasmModule>,
+        val: Value,
+        id: ProcessId,
+    },
+    // Scoped handlers for custom effects
+    //
+    // coord only (mainly because utxos can't resume utxos anyway)
+    InstallHandler {
+        interface_id: InterfaceId,
+    },
+    UninstallHandler {
+        interface_id: InterfaceId,
+    },
+    GetHandlerFor {
+        interface_id: InterfaceId,
+        handler_id: ProcessId,
+    },
+
+    // UTXO-only
+    Burn {
+        ret: Value,
+    },
+
+    Input {
+        val: Value,
+        caller: ProcessId,
+    },
+
+    // Tokens
+    Bind {
+        owner_id: ProcessId,
+    },
+    Unbind {
+        token_id: ProcessId,
+    },
+}
