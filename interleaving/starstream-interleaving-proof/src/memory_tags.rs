1d82aef09417a4619cd195e3149defc3819fe0bd
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2026-01-19 15:12:39 -0300

    chore: split circuit.rs (code re-org)
    Signed-off-by: Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>

---
+++ interleaving/starstream-interleaving-proof/src/memory_tags.rs
@@ -0,0 +1,80 @@
+use crate::F;
+use ark_r1cs_std::alloc::AllocVar;
+use ark_r1cs_std::fields::fp::FpVar;
+use ark_relations::gr1cs::{ConstraintSystemRef, SynthesisError};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MemoryTag {
+    // ROM tags
+    ProcessTable = 1,
+    MustBurn = 2,
+    IsUtxo = 3,
+    Interfaces = 4,
+
+    // RAM tags
+    ExpectedInput = 5,
+    Activation = 6,
+    Counters = 7,
+    Initialized = 8,
+    Finalized = 9,
+    DidBurn = 10,
+    Ownership = 11,
+    Init = 12,
+    RefArena = 13,
+    HandlerStackArenaProcess = 14,
+    HandlerStackArenaNextPtr = 15,
+    HandlerStackHeads = 16,
+    TraceCommitments = 17,
+}
+
+impl From<MemoryTag> for u64 {
+    fn from(tag: MemoryTag) -> u64 {
+        tag as u64
+    }
+}
+
+impl From<MemoryTag> for F {
+    fn from(tag: MemoryTag) -> F {
+        F::from(tag as u64)
+    }
+}
+
+impl MemoryTag {
+    pub fn allocate(&self, cs: ConstraintSystemRef<F>) -> Result<FpVar<F>, SynthesisError> {
+        FpVar::new_constant(cs, F::from(*self))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ProgramStateTag {
+    ExpectedInput,
+    Activation,
+    Init,
+    Counters,
+    Initialized,
+    Finalized,
+    DidBurn,
+    Ownership,
+}
+
+impl From<ProgramStateTag> for MemoryTag {
+    fn from(tag: ProgramStateTag) -> MemoryTag {
+        match tag {
+            ProgramStateTag::ExpectedInput => MemoryTag::ExpectedInput,
+            ProgramStateTag::Activation => MemoryTag::Activation,
+            ProgramStateTag::Init => MemoryTag::Init,
+            ProgramStateTag::Counters => MemoryTag::Counters,
+            ProgramStateTag::Initialized => MemoryTag::Initialized,
+            ProgramStateTag::Finalized => MemoryTag::Finalized,
+            ProgramStateTag::DidBurn => MemoryTag::DidBurn,
+            ProgramStateTag::Ownership => MemoryTag::Ownership,
+        }
+    }
+}
+
+impl From<ProgramStateTag> for u64 {
+    fn from(tag: ProgramStateTag) -> u64 {
+        let memory_tag: MemoryTag = tag.into();
+        memory_tag.into()
+    }
+}
