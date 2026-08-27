a0a6a06c3563d9d466a5f943b02f253205627910
Tad Hardesty <tad.hardesty@midnight.foundation>
2026-08-27 11:04:20 -0700

    Unignore score tests
    Signed-off-by: Tad Hardesty <tad.hardesty@midnight.foundation>

--- starstream-runtime-next/tests/score.rs
+++ starstream-runtime-next/tests/score.rs
@@ -324,7 +324,6 @@ async fn get_progress_storage<T: Send + 'static>(
 }
 
 #[test_log::test(tokio::test)]
-#[ignore = "dynamic UTXO imports unsupported"]
 async fn score_main_new() -> wasmtime::Result<()> {
     let engine = wasmtime::Engine::default();
     let contract =
@@ -444,7 +443,6 @@ async fn score_main_new() -> wasmtime::Result<()> {
 }
 
 #[test_log::test(tokio::test)]
-#[ignore = "dynamic UTXO imports unsupported"]
 async fn score_script_example() -> wasmtime::Result<()> {
     let engine = wasmtime::Engine::default();
     let contract =
